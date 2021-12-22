68efcba29fca54c98921d8562008a562537dbeec
Josh Zeltman <hea87617@adobe.com>
2021-12-22 11:34:31 -0300

    fixed bug in javascript startup

--- theme/demos/wireframe/theme.ts
+++ theme/demos/wireframe/theme.ts
@@ -1,6 +1,11 @@
 import languageNavigation from "./components/languagenavigation/languagenavigation"; "./components/languagenavigation/languagenavigation";
 
-
-document.addEventListener("DOMContentLoaded", (e) => {
+const documentReadyFunctions = (e) => {
   languageNavigation();
-});
+};
+
+if (document.readyState === "complete" || document.readyState === "interactive") {
+  setTimeout(documentReadyFunctions, 1);
+} else {
+  document.addEventListener("DOMContentLoaded", documentReadyFunctions);
+}
