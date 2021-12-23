5b83b9dc19b7784d45e408a1733dc10be1099b8f
Josh Zeltman <hea87617@adobe.com>
2021-12-23 11:16:44 -0300

    Moved progressbar js to demo folder. Removed Include and cleaned up theme.ts

--- theme/src/theme.ts
+++ theme/src/theme.ts
@@ -3,8 +3,6 @@ import "./theme.css";
 import "./resources/images/favicon.png";
 
 // Component Javascript
-import "./components/form-options/form_dropdown.js";
-import "./components/progressbar/progressbar.js";
 
 // Wireframe Demo JavaScript
 import "../demos/wireframe/theme";
