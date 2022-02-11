1cd76d62f15c7689bfcb80bb08207754fc5147ea
jzeltman <joshua.zeltman@gmail.com>
2022-02-11 11:45:20 -0300

    added images for the favicon and tailwind logo
    updated homepage feature with tailwind logo

--- docs/src/components/HomepageFeatures.js
+++ docs/src/components/HomepageFeatures.js
@@ -25,7 +25,7 @@ const FeatureList = [
   },
   {
     title: "Powered by Tailwind CSS",
-    Svg: require("../../static/img/undraw_docusaurus_react.svg").default,
+    Svg: require("../../static/img/tailwindcss.svg").default,
     description: (
       <>
         Extend or customize your website layout by reusing Tailwind CSS. EZ-AEM
