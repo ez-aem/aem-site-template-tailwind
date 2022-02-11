2586943ba59c4b1326206e926b7ffcf806cf4821
jzeltman <joshua.zeltman@gmail.com>
2022-02-11 18:48:43 -0300

    removed unused code

--- theme/src/color-scheme.ts
+++ theme/src/color-scheme.ts
@@ -1,15 +1,3 @@
-/* eslint no-empty: ["error", { "allowEmptyCatch": true }] */
-// try {
-//   if (
-//     localStorage.theme === "dark" ||
-//     (!("theme" in localStorage) &&
-//       window.matchMedia("(prefers-color-scheme: dark)").matches)
-//   ) {
-//     document.documentElement.classList.add("dark");
-//   } else {
-//     document.documentElement.classList.remove("dark");
-//   }
-// } catch (_) {}
 window
   .matchMedia("(prefers-color-scheme: dark)")
   .addEventListener("change", function (e) {
