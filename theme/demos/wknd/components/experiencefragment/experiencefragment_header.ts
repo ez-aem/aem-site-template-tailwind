0bb9e9b711fc8d47b58d6f40144fc75aa9f57448
jzeltman <joshua.zeltman@gmail.com>
2022-02-14 15:16:55 -0300

    added forced line endings via prettier
    removed prettier settings from eslint
    
    style tweaks
    
    added support for sticky header on scroll

--- theme/demos/wknd/components/experiencefragment/experiencefragment_header.ts
+++ theme/demos/wknd/components/experiencefragment/experiencefragment_header.ts
@@ -1,4 +1,5 @@
 const xfHeader = () => {
+  const $xfWrapper = document.querySelector("#header").parentElement;
   const $elWrapper = document.querySelector("#main-header-wrapper");
   const $el = document.querySelector("#main-header");
   const $button = document.querySelector("#mobile-header-button");
@@ -10,6 +11,17 @@ const xfHeader = () => {
     $elWrapper.classList.toggle("open");
   };
   $button.addEventListener("click", toggleEl);
+
+  // Add scroll listener
+  document.addEventListener("scroll", function (e) {
+    window.requestAnimationFrame(function () {
+      if (window.scrollY !== 0) {
+        document.documentElement.classList.add("scrolled");
+      } else {
+        document.documentElement.classList.remove("scrolled");
+      }
+    });
+  });
 };
 
 export default xfHeader;
