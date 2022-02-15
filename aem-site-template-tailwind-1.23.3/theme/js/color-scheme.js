13bfbded14a4aff08149228971c11e238976b218
jzeltman <joshua.zeltman@gmail.com>
2022-02-15 14:01:00 -0300

    removed errant inclusion of a test project

--- aem-site-template-tailwind-1.23.3/theme/js/color-scheme.js
+++
@@ -1,2 +0,0 @@
-(()=>{try{"dark"===localStorage.theme||!("theme"in localStorage)&&window.matchMedia("(prefers-color-scheme: dark)").matches?document.documentElement.classList.add("dark"):document.documentElement.classList.remove("dark")}catch(e){}})();
-//# sourceMappingURL=color-scheme.js.map
