f4156cbf2bc72560ef31d59ca7acdc3f69ea166f
jzeltman <joshua.zeltman@gmail.com>
2022-02-18 09:23:12 -0300

    added ez-aem logo
    updated homepage features to remove unauthored text

--- docs/src/components/HomepageFeatures.js
+++ docs/src/components/HomepageFeatures.js
@@ -5,31 +5,31 @@ import styles from "./HomepageFeatures.module.css";
 const FeatureList = [
   {
     title: "Easy to Use",
-    Svg: require("../../static/img/undraw_docusaurus_mountain.svg").default,
+    Svg: require("../../static/img/ez-aem-logo.svg").default,
     description: (
       <>
         EZ-AEM was designed from the ground up to be easily installed and used
-        to get your website up and running quickly.
+        to get your website up and running quickly. No developers required.
       </>
     ),
   },
-  {
-    title: "Focus on What Matters",
-    Svg: require("../../static/img/undraw_docusaurus_tree.svg").default,
-    description: (
-      <>
-        EZ-AEM lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
+  // {
+  //   title: "Focus on What Matters",
+  //   Svg: require("../../static/img/undraw_docusaurus_tree.svg").default,
+  //   description: (
+  //     <>
+  //       EZ-AEM lets you focus on your docs, and we&apos;ll do the chores. Go
+  //       ahead and move your docs into the <code>docs</code> directory.
+  //     </>
+  //   ),
+  // },
   {
     title: "Powered by Tailwind CSS",
     Svg: require("../../static/img/tailwindcss.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing Tailwind CSS. EZ-AEM
-        can be extended while reusing the same header and footer.
+        Gain speed and standardization by using the best-in-class CSS Library
+        with excellent tooling and documentation.
       </>
     ),
   },
@@ -37,7 +37,7 @@ const FeatureList = [
 
 function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx("col col--4")}>
+    <div className={clsx("col col--6")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} alt={title} />
       </div>
